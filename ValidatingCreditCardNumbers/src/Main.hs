0341242f2c03deaeb0e01cce3640e199fda43f76
francisco.peredo <francisco@extend.com.mx>
2013-08-24 23:25:11 -0500

    conversion de entero a lista de digitos completada... pero esta al reves

--- ValidatingCreditCardNumbers/src/Main.hs
+++ ValidatingCreditCardNumbers/src/Main.hs
@@ -1,11 +1,17 @@
 module Main where
 
-toDigits x = x
+toDigits :: Integer -> [Integer]
+
+toDigits x = (mod x 10) : toDigitList (mod x 10) (div x 10)
+             where toDigitList d 0 = []
+                   toDigitList d l = (mod l 10) : toDigitList (mod l 10) (div l 10)
+
+
 toDigitsRev x = x
 
 main::IO()
 main = do
        putStr "toDigits "
-       putStrLn (show (toDigits 1234))
+       putStrLn . show $ toDigits 1234
        putStr "toDigitsRev "
-       putStrLn (show (toDigitsRev 1234))
+       putStrLn . show $ toDigitsRev 1234
