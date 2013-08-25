854315a86fbbf57c336df1d46216301c33b4bc49
francisco.peredo <francisco@extend.com.mx>
2013-08-25 13:00:06 -0500

    doubleEveryOther listo

--- ValidatingCreditCardNumbers/src/Main.hs
+++ ValidatingCreditCardNumbers/src/Main.hs
@@ -1,12 +1,19 @@
 module Main where
 
 toDigitsRev :: Integer -> [Integer]
-
+-- como hacer esto con unfoldr?
 toDigitsRev  0 = []
 toDigitsRev  l = (mod (abs l) 10) : toDigitsRev  (div (abs l) 10)
 
 
 toDigits = reverse . toDigitsRev
+
+doubleEveryOtherRev :: [Integer] -> [Integer]
+doubleEveryOtherRev [] = []
+doubleEveryOtherRev l = last l : reallyDoubleEveryOther  (init l)
+                             where reallyDoubleEveryOther l = (last l) * 2 : doubleEveryOtherRev  (init l)
+
+doubleEveryOther = reverse . doubleEveryOtherRev
 
 main::IO()
 main = do
@@ -14,3 +21,5 @@ main = do
        putStrLn . show $ toDigits 1234
        putStr "toDigitsRev "
        putStrLn . show $ toDigitsRev 1234
+       putStr "doubleEveryOther "
+       putStrLn . show $ doubleEveryOther $ toDigits 1234
