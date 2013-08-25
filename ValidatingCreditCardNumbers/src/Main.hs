2fbf7b14eb9a71618c8891b96952d61c0ea70fbc
francisco.peredo <francisco@extend.com.mx>
2013-08-24 23:28:28 -0500

    mas simplificado, no se necesita una funcion definida en un where

--- ValidatingCreditCardNumbers/src/Main.hs
+++ ValidatingCreditCardNumbers/src/Main.hs
@@ -2,9 +2,8 @@ module Main where
 
 toDigits :: Integer -> [Integer]
 
-toDigits x = (mod x 10) : toDigitList  (div x 10)
-             where toDigitList  0 = []
-                   toDigitList  l = (mod l 10) : toDigitList  (div l 10)
+toDigits  0 = []
+toDigits  l = (mod l 10) : toDigits  (div l 10)
 
 
 toDigitsRev x = x
