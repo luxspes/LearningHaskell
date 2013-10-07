855f6ad5c78d9369305d87174f24cee6ae7f9f8e
Francisco Peredo <luxspes@yahoo.com>
2013-10-06 20:38:16 -0500

    uso de datetype para los Pegs

--- HanoiTowers/src/Main.hs
+++ HanoiTowers/src/Main.hs
@@ -3,15 +3,15 @@ module Main where
 
 type Peg = String
 type Move = (Peg, Peg)
-type Pegs = ((Peg,[Integer]),(Peg,[Integer]),(Peg,[Integer]))
+data Pegs = Pegs { p1::(Peg,[Integer]), p2::(Peg,[Integer]) ,p3::(Peg,[Integer]) } deriving (Show)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi disks t1 t2 t3 =  getmoves ((t1,[1..disks]),(t2,[]),(t3,[]))
+hanoi disks t1 t2 t3 =  getmoves Pegs {p1 = (t1,[1..disks]), p2 = (t2,[]), p3 =  (t3,[])}
 
 getmoves:: Pegs->[Move]
 getmoves pegs  = []
 
 getmove:: Pegs->(Move,Pegs)
-getmove pegs  = (("",""), (("",[]),("",[]),("",[])) )
+getmove pegs  = (("",""), Pegs ("",[]) ("",[]) ("",[]) )
 
 
 main::IO()
