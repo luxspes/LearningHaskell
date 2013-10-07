ebbe3c76f4c20a9eda47ffda42b17b4015617d8b
Francisco Peredo <luxspes@yahoo.com>
2013-10-06 22:00:31 -0500

    haciendo match con un guard

--- HanoiTowers/src/Main.hs
+++ HanoiTowers/src/Main.hs
@@ -11,9 +11,11 @@ getmoves:: Pegs->[Move]
 getmoves pegs  = []
 
 getmove:: Pegs->(Move,Pegs)
-getmove pegs  = (("",""), Pegs ("",[]) ("",[]) ("",[]) )
+getmove pegs  -- = (("",""), Pegs ("",[]) ("",[]) ("",[]) )
+  | length (snd(p1 pegs)) == 1 && length (snd(p2 pegs)) == 0 && length (snd(p3 pegs)) == 0 =  (("",""), Pegs ("",[]) ("",[]) ("",[]) )
+  | otherwise = (("",""), Pegs ("",[]) ("",[]) ("",[]) )
 
 
 main::IO()
 main = do
-  print $ hanoi 3 "t1" "t2" "t3"
+  print $ hanoi 1 "t1" "t2" "t3"
