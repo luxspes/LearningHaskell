2c7a82476dbdcc55af1782560690f7296251119d
Francisco Peredo <luxspes@yahoo.com>
2013-10-08 14:52:54 -0500

    quito el tipo "Pegs" pongo el tipo Peg y uso listas de Peg

--- HanoiTowers/src/Main.hs
+++ HanoiTowers/src/Main.hs
@@ -3,18 +3,18 @@ module Main where
 
 type PegName = String
 type Move = (PegName, PegName)
-data Pegs = Pegs { p1::(PegName,[Integer]), p2::(PegName,[Integer]) ,p3::(PegName,[Integer]) } deriving (Show)
+type Peg = (PegName,[Integer])
 hanoi :: Integer -> PegName -> PegName -> PegName -> [Move]
-hanoi disks t1 t2 t3 =  getmoves Pegs {p1 = (t1,[1..disks]), p2 = (t2,[]), p3 =  (t3,[])}
+hanoi disks t1 t2 t3 =  getmoves [(t1,[1..disks]),(t2,[]),(t3,[])]
 
-getmoves:: Pegs->[Move]
+getmoves:: [Peg]->[Move]
 getmoves pegs  = []
 
-makeMove:: Pegs->Move->Pegs
+makeMove:: [Peg]->Move->[Peg]
 makeMove pegs move = pegs
 
 
 main::IO()
 main = do
-  print (Pegs ("t1",[1,2]) ("t2",[]) ("t3",[]))
-  print $ makeMove (Pegs ("t1",[1,2]) ("t2",[]) ("t3",[])) ("t1","t2");
+  print [ ("t1",[1,2]),("t2",[]),("t3",[])]
+  print $ makeMove [("t1",[1,2]),("t2",[]),("t3",[])] ("t1","t2");
