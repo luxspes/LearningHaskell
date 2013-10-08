7dded337de12e141cb30b18f6deaa1cb141e3161
Francisco Peredo <luxspes@yahoo.com>
2013-10-08 16:29:34 -0500

    nomenclatura ajustada

--- PlayingWithLists/src/Main.hs
+++ PlayingWithLists/src/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
-list1 = [1,2,3,4]
+listaSimple = [1,2,3,4]
+listaDeTuplas = [("a",1),("b",2),("c",3),("d",4)]
 
 main::IO()
 main = do
-        print list1
-        print $  reverse list1 
-        print $ filter odd list1 
-        print $ filter (\x->x==4) list1
+        print listaSimple
+        print $ reverse listaSimple 
+        print $ filter odd listaSimple 
+        print $ filter (\x->x==4) listaSimple
+        print listaDeTuplas
